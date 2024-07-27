2bcc68fc3e502638cf73394fb04ef521a5d91f4c
Faraz Khan <farazkhanaa2555@gmail.com>
2024-07-27 17:39:42 +0500

    refactor: update code context

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend domain
+    origin: "http://localhost:3000", // Replace with Your frontend domain
     credentials: true,
   })
 );
